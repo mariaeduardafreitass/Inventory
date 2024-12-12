4ec6f838cf5f8ebb6a42ee0022a595a3b39e0c77
Aluno <Aluno@10.85.198.9>
2024-12-12 19:06:39 -0300

    Seção 6: Criando um Projeto de Inventário

--- src/inventory/ProductTester.java
+++ src/inventory/ProductTester.java
@@ -1,10 +1,11 @@
 
 package inventory;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ProductTester { 
-    public static void main(String[] args) {
+public class ProductTester {
+public static void main(String[] args) {
 //criar um objeto Scanner para entrada do teclado
 Scanner in = new Scanner(System.in);
 //criar variáveis locais
@@ -12,19 +13,37 @@ int tempNumber;
 String tempName;
 int tempQty;
 double tempPrice;
-//pedir ao usuário para inserir as informações sobre o produto
-System.out.print("\n\nInsira o nome do produto: ");
-tempName = in.nextLine();
-System.out.print("Insira a quantidade em estoque para este produto: ");
-tempQty = in.nextInt();
-System.out.print("Insira o preço para este produto: ");
-tempPrice = in.nextDouble();
-System.out.print("Insira o número do item: ");
-tempNumber = in.nextInt();
-//crie o primeiro produto usando as informações inseridas pelo usuário
-Product p1 = new Product(tempNumber, tempName, tempQty, tempPrice);
-//pedir ao usuário para inserir as informações sobre o produto
+int maxSize=-1;
+//solicitar que o usuário insira um número >= zero
+do {
+try{
+//exibir prompt para o usuário
+System.out.println("Insira o número de produtos que gostaria de adicionar.");
+System.out.print("Insira 0 (zero) se não quiser adicionar produtos: ");
+//supor que o usuário informe um valor válido
+maxSize = in.nextInt();
+if(maxSize<0)
+System.out.println("Valor Incorreto inserido");
+//endif
+}
+catch(InputMismatchException e) {
+System.out.println("Tipo incorreto de dados inserido!");
 in.nextLine();
+}
+catch(Exception e){
+System.out.println(e);
+in.nextLine();
+}
+}while(maxSize<0);
+if(maxSize ==0) {
+//Exibir uma mensagem de que não há produto se zero for inserido
+System.out.println("Não há produtos!");
+}else {
+Product[] products = new Product[maxSize];
+for(int i = 0; i<maxSize; i++) {
+in.nextLine();
+//pedir ao usuário para inserir as informações sobre o produto
+
 System.out.print("\n\nInsira o nome do produto: ");
 tempName = in.nextLine();
 System.out.print("Insira a quantidade em estoque para este produto: ");
@@ -33,25 +52,13 @@ System.out.print("Insira o preço para este produto: ");
 tempPrice = in.nextDouble();
 System.out.print("Insira o número do item: ");
 tempNumber = in.nextInt();
-//criar dois objetos usando o construtor padrão
-Product p2 = new Product(tempNumber, tempName, tempQty, tempPrice);
-//fechar o objeto Scanner
-in.close();
-//criar quatro objetos usando valores para os argumentos
-//isso utilizará o construtor sobrecarregado
-Product p3 = new Product(3, "Hot Shots", 75, 8.99);
-Product p4 = new Product(4, "Brothers In Arms", 200, 7.75);
-Product p5 = new Product(5, "Fantastic", 500, 9.50);
-Product p6 = new Product(6, "The Party Album", 150, 4.00);
-//definir o campo Ativo como falso
-
-p6.setActive(false);
-//exibir a descrição de cada objeto conforme descrito no método toString()
-System.out.println(p1);
-System.out.println(p2);
-System.out.println(p3);
-System.out.println(p4);
-System.out.println(p5);
-System.out.println(p6);
+//criar um objeto Produto e armazená-lo no array de produtos
+products[i] = new Product(tempNumber, tempName, tempQty, tempPrice);
+}//endfor
+//exibir o conteúdo do array de produtos
+for(Product product: products)
+System.out.println(product);
+//endor
+}//endif
 }//encerrar método principal
 }//encerrar classe ProductTester
